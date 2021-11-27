c1334ceed8f3282059ec40a65cb67544d15a4da8
ekinohito <zhenyagur9@gmail.com>
2021-11-27 10:48:53 +0300

    gif previews

--- content_script.js
+++ content_script.js
@@ -1,33 +1,45 @@
 // Put all the javascript code here, that you want to execute after page load
-const wrapper = document.createElement('div')
-wrapper.className = "wrapper"
+async function addTenor() {
+    const html = document.createElement('div')
+    html.innerHTML = (await (await fetch(browser.runtime.getURL("tenor.html"))).text())
+    document.body.prepend(html.firstChild)
 
-const tenorMenu = document.createElement('div')
-tenorMenu.className = "tenor-menu"
-tenorMenu.textContent = "VK Tenor"
+    const wrapper = document.querySelector(".vk-tenor")
+    const button = wrapper.querySelector(".button")
+    const popup = wrapper.querySelector(".popup")
+    const searchbar = wrapper.querySelector(".searchbar")
+    const gifbins = wrapper.getElementsByClassName("gifbin")
+    const gifTemplate = wrapper.querySelector(".gif-template")
 
-const searchbar = document.createElement('input')
-searchbar.className = "searchbar"
-searchbar.placeholder = "Search Tenor"
-searchbar.addEventListener("change", async () => {
-    console.log(searchbar.value)
+    button.addEventListener("click", () => { popup.classList.toggle("hidden") })
+    wrapper.addEventListener("click", (e) => { e.stopPropagation() })
+    document.body.addEventListener("click", () => popup.classList.add("hidden"))
+
+    let gifs = null
+    function setGifs(newGifs) {
+        gifs = newGifs
+        for (const gifbin of gifbins) {
+            gifbin.innerHTML = ""
+        }
+        let currentBin = 0
+        for (const gif of gifs) {
+            const gifElement = gifTemplate.content.firstElementChild.cloneNode(true);
+            gifElement.src = gif.media[0].tinygif.url
+            gifbins[currentBin++].append(gifElement)
+            currentBin = currentBin % gifbins.length
+        }
+    }
+    searchbar.addEventListener("change", async () => setGifs(await fetchGifs(searchbar.value)))
+}
+
+async function fetchGifs(query) {
     try {
-        const res = await fetch(`https://g.tenor.com/v1/search?q=${searchbar.value}&key=S30OUUVPB3T5&limit=8`)
+        const res = await fetch(`https://g.tenor.com/v1/search?q=${query}&key=S30OUUVPB3T5&limit=8&contentfilter=high&media_filter=minimal`)
         const data = await res.json()
-        console.log(data)
+        return data.results
     } catch (e) {
         console.log(e.message)
     }
+}
 
-})
-tenorMenu.appendChild(searchbar)
-
-const button = document.createElement('button')
-button.textContent = "Open menu"
-button.addEventListener("click", () => {tenorMenu.style.display = "flex" })
-wrapper.addEventListener("click", (e) => { e.preventDefault(); e.stopPropagation(); })
-document.body.addEventListener("click", () => tenorMenu.style.display = "none")
-
-wrapper.appendChild(button)
-wrapper.appendChild(tenorMenu)
-document.body.prepend(wrapper)
+addTenor()
